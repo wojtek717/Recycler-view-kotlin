9dbcde0f0233e0516089b6b5d33d5cd7273ff07a
wojtek717 <wojciechkonury@gmail.com>
2019-01-03 13:34:12 +0100

    First commit

---
+++ app/src/main/java/com/example/wojciechkonury/kotlinrecyclerview/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.wojciechkonury.kotlinrecyclerview
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        recyclerView_main.layoutManager = LinearLayoutManager(this)
+        recyclerView_main.adapter = MainAdapter()
+    }
+}
---
+++ app/src/main/java/com/example/wojciechkonury/kotlinrecyclerview/MainAdapter.kt
@@ -0,0 +1,34 @@
+package com.example.wojciechkonury.kotlinrecyclerview
+
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import kotlinx.android.synthetic.main.video_row.view.*
+
+class MainAdapter : RecyclerView.Adapter<CustomVievHolder>(){
+
+    val videoTitles = listOf<String>("First", "Second", "Third", "One more!", "AND ONE MORE MORE!")
+
+    override fun getItemCount(): Int {
+        return videoTitles.size
+    }
+
+    override fun onCreateViewHolder(p0: ViewGroup, p1: Int): CustomVievHolder {
+        val layoutInflater = LayoutInflater.from(p0.context)
+        val cellForRow = layoutInflater.inflate(R.layout.video_row, p0, false) //Tutaj wstawiamy plik layout
+
+        return CustomVievHolder(cellForRow)
+    }
+
+    override fun onBindViewHolder(p0: CustomVievHolder, p1: Int) {
+        val videoTitle = videoTitles.get(p1)
+
+        p0.view.textView_video_title.text = videoTitle
+    }
+}
+
+class CustomVievHolder(val view: View) : RecyclerView.ViewHolder(view)
+{
+
+}
